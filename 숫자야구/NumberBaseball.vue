f187c1a7fec847ef582484de8cb006a58ca77a42
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-12-04 23:24:04 +0900

    숫자야구 웹팩 세팅
    에러... 나중에 다시 시도 예정

---
+++ 숫자야구/NumberBaseball.vue
@@ -0,0 +1,23 @@
+<template>
+    <form @submit="onSubmitForm">
+        <h1>{{ result }}</h1>
+        <input type="number" maxlength="4" v-model="value" ref="answer" />
+        <button type="submit">입력</button>
+        <div>시도 {{ leftTryNumber }}</div>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+
+        },
+        methods: {
+
+        }
+    };
+</script>
+
+<style>
+
+</style>
---
+++ 숫자야구/webpack.config.js
@@ -0,0 +1,26 @@
+// 현재 폴더 경로 받아오는 함수
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    entry: {
+        app: './main.js',
+
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader',
+        }],
+
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+
+    ],
+    output: {
+        filename: 'app.js',
+        path: path.join(__dirname, './dist'),
+
+    },
+};
