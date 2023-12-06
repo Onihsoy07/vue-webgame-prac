eab19e445ce6701deb76432384116ce24172e1cf
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-12-06 11:29:21 +0900

    숫자야구 webpack test(vue-template-compiler 없이?)

---
+++ number-baseball1/NumberBaseball.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <form @submit="onSubmitForm">
+            <div>{{ result }}</div>
+            <input type="number" v-model="value" ref="answer" maxlength="4" />
+            <button type="submit">입력</button>
+            <div>{{ leftTry }}</div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NumberBaseball',
+        data() {
+            return {
+                value: '',
+                result: '',
+                leftTry: '',
+
+            }
+        },
+        methods: {
+            onSubmitForm(e) {
+                e.preventDefault();
+            }
+        },
+        props: {
+
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
