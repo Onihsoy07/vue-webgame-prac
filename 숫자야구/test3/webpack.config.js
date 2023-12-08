739fa2df374abf06c40fe1ef0b9713172f188acb
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-12-08 22:48:35 +0900

    추가 설정

--- 숫자야구/test3/webpack.config.js
+++ 숫자야구/test3/webpack.config.js
@@ -3,6 +3,12 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 // const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
+    model: 'development',
+    devtool: 'eval',
+    resolve: {
+        extenstions: ['.js', '.vue'],
+        
+    },
     entry: {
         app: path.join(__dirname, 'main.js'),
 
