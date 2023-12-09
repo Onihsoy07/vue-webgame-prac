bd5fd138de4cf02f7541e03c7716c40835331327
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-12-09 23:46:23 +0900

    vue3로 숫자야구 일부 구현

---
+++ 숫자야구/number-baseball/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <NumberBaseball></NumberBaseball>
+</template>
+
+<script>
+import NumberBaseball from './components/NumberBaseball.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NumberBaseball,
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 숫자야구/number-baseball/src/components/NumberBaseball.vue
@@ -0,0 +1,50 @@
+<template>
+    <form @submit.prevent="onSubmitForm">
+        <input type="number" minlength="4" maxlength="4" v-model="value" ref="answer" />
+        <button>입력</button>
+        <div>시도 : {{ tries.length }}</div>
+        <ul>
+            <li v-for="(t, idx) in tries" :key="idx">
+                <div>{{ t.try }}</div>
+                <div>{{ t.result }}</div>
+            </li>
+        </ul>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'NumberBaseball',
+    data() {
+        return {
+            value: '',
+            result: '',
+            tries: [],
+
+        }
+    },
+    methods: {
+        onSubmitForm() {
+            this.tries.push({
+                try: this.value,
+                result: 'result'
+            });
+            this.value = '';
+            this.$refs.answer.focus();
+        },
+
+    },  
+}
+</script>
+
+<style scoped>
+form {
+    max-width: 300px;
+    margin: 0 auto;
+}
+ul {
+    max-width: 100px;
+    margin: 0 auto;
+}
+
+</style>
