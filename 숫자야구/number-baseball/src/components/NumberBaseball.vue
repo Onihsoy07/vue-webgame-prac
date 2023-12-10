557bc69336ccf2175429293e9637011cb00ead2b
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-12-11 00:02:15 +0900

    숫자게임 로직 구현

--- 숫자야구/number-baseball/src/components/NumberBaseball.vue
+++ 숫자야구/number-baseball/src/components/NumberBaseball.vue
@@ -1,6 +1,6 @@
 <template>
     <form @submit.prevent="onSubmitForm">
-        <input type="number" minlength="4" maxlength="4" v-model="value" ref="answer" />
+        <input type="text" minlength="4" maxlength="4" v-model="value" ref="answer" />
         <button>입력</button>
         <div>시도 : {{ tries.length }}</div>
         <ul>
@@ -18,19 +18,59 @@ export default {
     data() {
         return {
             value: '',
-            result: '',
             tries: [],
+            initNumbers: this.getRandomNumbers(),
 
         }
     },
     methods: {
         onSubmitForm() {
-            this.tries.push({
-                try: this.value,
-                result: 'result'
-            });
-            this.value = '';
-            this.$refs.answer.focus();
+            if(this.value == this.initNumbers.join('')) {
+                    this.tries.push({
+                    try: this.value,
+                    result: '정답'
+                });
+                alert('다시 시작합니다');
+                this.value = '';
+                this.tries= [];
+                this.$refs.answer.focus();
+                this.initNumbers = this.getRandomNumbers();
+            } else {
+                let strike = 0;
+                let ball = 0;
+                for(let i = 0; i < 4; i++) {
+                    if(this.value[i] == this.initNumbers[i]) {
+                        strike++;
+                    } else if(this.initNumbers.includes(parseInt(this.value[i]))) {
+                        ball++;
+                    }
+                }
+                this.tries.push({
+                    try: this.value,
+                    result: `${strike} 스트라이크, ${ball} 볼`
+                });
+                this.value = '';
+                this.$refs.answer.focus();
+            }
+            if(this.tries.length >= 10) {
+                alert(`실패하였습니다. 정답은 ${this.initNumbers.join('')}입니다.`);
+                this.value = '';
+                this.tries= [];
+                this.$refs.answer.focus();
+                this.initNumbers = this.getRandomNumbers();
+                return;
+            }
+
+
+        },
+        getRandomNumbers() {
+            const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            const array = [];
+            for(let i = 0; i < 4; i++) {
+                const choiceNumber = numbers.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+                array.push(choiceNumber);
+            }
+            return array;
         },
 
     },  
@@ -43,7 +83,7 @@ form {
     margin: 0 auto;
 }
 ul {
-    max-width: 100px;
+    max-width: 150px;
     margin: 0 auto;
 }
 
