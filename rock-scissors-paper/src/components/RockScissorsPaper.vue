612e3d859490f2f56748c1fc11bb043e7066860b
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-12-14 23:50:41 +0900

    가위바위보 에러
    Cannot read properties of undefined (reading 'imageStartXAxis')
    TypeError: Cannot read properties of undefined (reading 'imageStartXAxis')

--- rock-scissors-paper/src/components/RockScissorsPaper.vue
+++ rock-scissors-paper/src/components/RockScissorsPaper.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div id="computer" :style="{ computedStyleObject, width: '150px', height: '200px', margin: '0 auto' }" :class="{}"></div>
+        <div id="computer" :style="computedStyleObject" :class="{}"></div>
         <div>
             <button @click="onClickButton('바위')">바위</button>
             <button @click="onClickButton('가위')">가위</button>
@@ -18,33 +18,67 @@ export default {
         return {
             result: '',
             score: '',
-            imageStartXAxis: '0px',
             rspStartXAxis: {
-                rock: '0',
+                rock: '0px',
                 scissors: '-135px',
                 paper: '-284px',
             },
+            ccol: {
+                red: 'red'
+            },
+            imageStartXAxis: '',
+            interval: null,
 
         }
     },
     computed: {
         computedStyleObject() {
             return {
-                background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imageStartXAxis} 0`
+                background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imageStartXAxis} 0`,
+                width: '150px', height: '200px', margin: '0 auto'
             };
         },
 
     },
     methods: {
         onClickButton(choice) {
+            clearInterval(this.interval);
+            console.log(this.computerChoice());
+            console.log(choice);
             return choice;
-        }
+        },
+        computerChoice() {
+            Object.entries(this.rspStartXAxis).find(function(v) {
+                if(v.includes[this.imageStartXAxis]) {
+                    console.log(v);
+                }
+            });
+            return null;
+        },
+    },
+    created() {
+        console.log('created');
     },
     mounted() {
-        console.log(this.rspStartXAxis.보);
-        console.log(this.imageStartXAxis);
-
-    }
+        this.imageStartXAxis = this.rspStartXAxis.rock;
+        console.log('mounted');
+        this.interval = setInterval(() => {
+            if(this.imageStartXAxis === this.rspStartXAxis.rock) {
+                this.imageStartXAxis = this.rspStartXAxis.scissors;
+            } else if(this.imageStartXAxis === this.rspStartXAxis.scissors) {
+                this.imageStartXAxis = this.rspStartXAxis.paper;
+            } else if(this.imageStartXAxis === this.rspStartXAxis.paper) {
+                this.imageStartXAxis = this.rspStartXAxis.rock;
+            }
+        }, 70);
+    },
+    updated() {
+        console.log('updated');
+    },
+    beforeUnmount() {
+        console.log('beforeUnmount');
+        clearInterval(this.interval);
+    },
 
 };
 </script>
