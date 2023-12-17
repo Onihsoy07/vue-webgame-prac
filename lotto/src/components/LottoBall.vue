89567a4e6cd0135b02a93f1167599e77252067db
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-12-17 23:37:45 +0900

    로또 추첨 컴포넌트 프레임 구성
    로또 제너레이터, 로또 볼 컴포넌ㅌ

---
+++ lotto/src/components/LottoBall.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="ball" :style="{ background }">
+        {{ number }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LottoBall',
+    data() {
+        return {
+            background: '',
+            
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    props: {
+        number: Number,
+    }
+};
+</script>
+
+<style scoped>
+.ball {
+    display: inline-block;
+    border: 1px solid black;
+    border-radius: 20px;
+    width: 40px;
+    height: 40px;
+    line-height: 40px;
+    font-size: 20px;
+    text-align: center;
+    margin-right: 20px;
+}
+</style>
---
+++ lotto/src/components/LottoGenerator.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div>당첨 숫자</div>
+        <div id="result-window">
+            <lotto-ball v-for="(ball, idx) in lottoBalls" :key="idx" :number="ball"></lotto-ball>
+        </div>
+        <div>보너스</div>
+        <lotto-ball v-if="bonusBall"></lotto-ball>
+        <button v-if="redo">다시 하기</button>
+    </div>
+</template>
+
+<script>
+import LottoBall from './LottoBall.vue';
+
+function getLottoBalls() {
+    return null;
+}
+
+export default {
+    name: 'LottoGenerator',
+    data() {
+        return {
+            redo: false,
+            lottoBalls: getLottoBalls(),
+            bonusBall: null,
+
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    watch: {
+        
+    },
+    components: {
+        LottoBall,
+
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
