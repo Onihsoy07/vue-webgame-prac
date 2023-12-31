ff9479615b7188d4b31605cf1cf80c8668632d73
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-12-31 23:02:24 +0900

    vuex mapState 사용 문제
    기본 script setup에서 안됨?
    export default 변경하였지만 undefined

--- tic-tac-toe-vuex/src/components/TdComponent.vue
+++ tic-tac-toe-vuex/src/components/TdComponent.vue
@@ -1,66 +1,87 @@
 <template>
-    <td @click="onClickTd">{{ tableDataCom[props.rowIdx][props.cellIdx] }}</td>
+    <td @click="onClickTd">
+        <!-- {{ tableData[this.rowIdx][this.cellIdx] }} -->
+    </td>
 </template>
 
-<script setup>
-import { useStore } from 'vuex';
-import { defineProps, computed } from 'vue';
+<script>
+import { useStore, mapState } from 'vuex';
+// import { computed } from 'vue';
 
-const store = useStore();
-const tableDataCom = computed(() => {
-    return store.state.tableData;
-});
-const turn = computed(() => {
-    return store.state.turn;
-});
-const turnCount = computed(() => {
-    return store.state.turnCount;
-});
+export default {
+    setup() {
+        const store = useStore();
+        // const tableDataCom = computed(() => store.state.tableData);
+        // const turn = computed(() => store.state.turn);
+        // const turnCount = computed(() => store.state.turnCount);
 
-let props = defineProps({
-    rowIdx: Number,
-    cellIdx: Number,
+        // const storeData = computed(() => {
+        //     return mapState(['tableData', 'turn', 'turnCount']);
+        // });
 
-});
+        // let props = defineProps({
+        //     rowIdx: Number,
+        //     cellIdx: Number,
+
+        // });
 
 
-const onClickTd = () => {
-    if(tableDataCom.value[props.rowIdx][props.cellIdx] === '') {
-        store.commit('CLICK_CELL', { row : props.rowIdx, cell : props.cellIdx });
-        store.commit('UP_TURNCOUNT');
+        const onClickTd = () => {
+            if(this.tableData[this.rowIdx][this.cellIdx] === '') {
+                store.commit('CLICK_CELL', { row : this.rowIdx, cell : this.cellIdx });
+                store.commit('UP_TURNCOUNT');
 
-        let win = false;
-        const tableData = store.state.tableData;
+                // let win = false;
+                // const tableData = store.state.tableData;
 
-        if(tableData[props.rowIdx][0] === turn.value && tableData[props.rowIdx][1] === turn.value && tableData[props.rowIdx][2] === turn.value) {
-            win = true;
+                // if(tableData[this.rowIdx][0] === this.turn && tableData[this.rowIdx][1] === this.turn && tableData[this.rowIdx][2] === this.turn) {
+                //     win = true;
+                // }
+                // if(tableData[0][this.cellIdx] === this.turn && tableData[1][this.cellIdx] === this.turn && tableData[2][this.cellIdx] === this.turn) {
+                //     win = true;
+                // }
+                // if(tableData[0][0] === this.turn && tableData[1][1] === this.turn && tableData[2][2] === this.turn) {
+                //     win = true;
+                // }
+                // if(tableData[2][0] === this.turn && tableData[1][1] === this.turn && tableData[0][2] === this.turn) {
+                //     win = true;
+                // }
+                
+                // if(win) {
+                //     store.commit('SET_RESULT', this.turn + '승리');
+                //     store.commit('RESET_GAME');
+                //     return win;
+                // }
+
+                // if(this.turnCount === 9) {
+                //     store.commit('SET_RESULT', '무승부');
+                //     store.commit('RESET_GAME');
+                //     return;
+                // }
+
+                // store.commit('CHANGE_TURN');
+            }
         }
-        if(tableData[0][props.cellIdx] === turn.value && tableData[1][props.cellIdx] === turn.value && tableData[2][props.cellIdx] === turn.value) {
-            win = true;
-        }
-        if(tableData[0][0] === turn.value && tableData[1][1] === turn.value && tableData[2][2] === turn.value) {
-            win = true;
-        }
-        if(tableData[2][0] === turn.value && tableData[1][1] === turn.value && tableData[0][2] === turn.value) {
-            win = true;
-        }
-        
-        if(win) {
-            console.log('win')
-            store.commit('SET_RESULT', turn.value + '승리');
-            store.commit('RESET_GAME');
-            return win;
-        }
 
-        if(turnCount.value === 9) {
-            store.commit('SET_RESULT', '무승부');
-            store.commit('RESET_GAME');
-            return;
+        return {
+            onClickTd,
         }
-
-        store.commit('CHANGE_TURN');
+    },
+    computed: {
+        ...mapState(['tableData', 'turn', 'turnCount'])
+    },
+    props: {
+        rowIdx: Number,
+        cellIdx: Number,
+    },
+    created() {
+        console.log(tableData);
+        console.log(turn);
+        console.log(turnCount);
     }
-}
+};
+
+
 
 </script>
 
